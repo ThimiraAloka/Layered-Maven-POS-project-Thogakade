f6197697fdd8ef07c036845abbe3ab163212cc94
Thimira Aloka M H <150279156+ThimiraAloka@users.noreply.github.com>
2023-12-22 10:54:13 +0530

    CustomerDaoImpl completed

--- src/main/java/dao/custom/CustomerDao.java
+++ src/main/java/dao/custom/CustomerDao.java
@@ -8,11 +8,4 @@ import java.sql.SQLException;
 import java.util.List;
 
 public interface CustomerDao extends CrudDao<Customer> {
-    boolean saveCustomer(CustomerDto dto) throws SQLException, ClassNotFoundException;
-    boolean updateCustomer(CustomerDto dto) throws SQLException, ClassNotFoundException;
-    boolean deleteCustomer(String id) throws SQLException, ClassNotFoundException;
-    List<CustomerDto> allCustomers() throws SQLException, ClassNotFoundException;
-    CustomerDto searchCustomer(String id);
-
-
 }
