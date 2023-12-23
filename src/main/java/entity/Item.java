c7e6a1e6fa4a0306a71947485cccc5853fac57c6
Thimira Aloka M H <150279156+ThimiraAloka@users.noreply.github.com>
2023-12-24 00:30:06 +0530

    add sessionFractory method body

--- src/main/java/entity/Item.java
+++ src/main/java/entity/Item.java
@@ -2,12 +2,17 @@ package entity;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @ToString
+@Entity
 public class Item {
+    @Id
     private String code;
     private String desc;
     private double unitPrice;
--- src/main/java/entity/Orders.java
+++ src/main/java/entity/Orders.java
@@ -2,12 +2,17 @@ package entity;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @ToString
+@Entity
 public class Orders {
+    @Id
     private String orderId;
     private String date;
     private String custId;
